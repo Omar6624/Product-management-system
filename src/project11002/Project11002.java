537e308509848574d572e1fd22b9be62536ceb7d
Omar6624 <86346592+Omar6624@users.noreply.github.com>
2024-04-10 03:05:46 +0600

    fist

---
+++ src/project11002/Project11002.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project11002;
+
+/**
+ *
+ * @author asus
+ */
+public class Project11002 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       firstpage fp = new firstpage();
+       fp.setVisible(true);
+       
+       
+    }
+    
+}
